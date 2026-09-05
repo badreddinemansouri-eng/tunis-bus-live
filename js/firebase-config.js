f1e8403b10d26fc189a215eafb793b622b3099f2
badreddinemansouri-eng <badreddine.mansouri@etudiant-fst.utm.tn>
2026-09-05 21:44:02 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,13 @@
 const firebaseConfig = {
   apiKey: "AIzaSyABoGayaVI3Tl-3zbc6kWpz9U5b63eBq8s",
   authDomain: "tunis-bus-live.firebaseapp.com",
-  databaseURL: "https://tunis-bus-live-default-rtdb.firebaseio.com",   // ← THIS ONE WORKS
+  databaseURL: "https://tunis-bus-live-default-rtdb.europe-west1.firebasedatabase.app",   // ← CHANGE TO THIS
   projectId: "tunis-bus-live",
   storageBucket: "tunis-bus-live.firebasestorage.app",
   messagingSenderId: "205154018267",
   appId: "1:205154018267:web:b954b5df086c3e6e8e3dfe",
   measurementId: "G-3SLJKW1BEC"
 };
+
+// Make sure we initialize the app
+firebase.initializeApp(firebaseConfig);
