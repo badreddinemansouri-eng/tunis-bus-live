3c5111537563170f36e1661090ae9dea03f4e220
badreddinemansouri-eng <badreddine.mansouri@etudiant-fst.utm.tn>
2026-09-05 14:46:39 +0100

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABoGayaVI3Tl-3zbc6kWpz9U5b63eBq8s",
+  authDomain: "tunis-bus-live.firebaseapp.com",
+  databaseURL: "https://tunis-bus-live-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tunis-bus-live",
+  storageBucket: "tunis-bus-live.firebasestorage.app",
+  messagingSenderId: "205154018267",
+  appId: "1:205154018267:web:b954b5df086c3e6e8e3dfe",
+  measurementId: "G-3SLJKW1BEC"
+};
+
+// Initialize Firebase (using the compat library already loaded in index.html)
+firebase.initializeApp(firebaseConfig);
